23356f0afeb4d5606add4e1b341e69a07c150937
Başak Acar <acarbasak4@gmail.com>
2020-12-28 16:51:29 +0300

    links

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Colors } from "./components/Colors";
@@ -9,6 +9,22 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+
+            <li>
+              <Link to={"/about"}>About</Link>
+            </li>
+
+            <li>
+              <Link to={"/colors"}>Colors</Link>
+            </li>
+          </ul>
+        </nav>
+
         <Switch>
           <Route path="/" exact component={Home}></Route>
           <Route path="/about" component={About}></Route>
