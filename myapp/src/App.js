083695c90ec276c6226b216d3430b054e38cfa46
Başak Acar <acarbasak4@gmail.com>
2020-12-28 16:26:21 +0300

    -

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
+
+    <Route path ="/" component={Home}></Route>
+    <Route path ="/about" component={About}></Route>
+    <Route path ="/colors" component={Colors}></Route>
       
     </div>
+    </Router>
   );
 }
 
