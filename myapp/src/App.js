f588fe1ba39ea0bcbd86a8250c899753b123b6f8
Başak Acar <acarbasak4@gmail.com>
2020-12-28 16:40:42 +0300

    switch-exact

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Colors } from "./components/Colors";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-
-    <Route path ="/" component={Home}></Route>
-    <Route path ="/about" component={About}></Route>
-    <Route path ="/colors" component={Colors}></Route>
-      
-    </div>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/colors" component={Colors}></Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
---
+++ myapp/src/components/Colors.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Colors =()=> <div>Colors Component</div>
